95eec6c7efd1706de17e567e8b57078547bd3f26
IrenMarkoni <77893647+IrenMarkoni@users.noreply.github.com>
2021-06-30 12:35:20 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php
+// Откликаться будет ТОЛЬКО на ajax запросы
+if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || $_SERVER['HTTP_X_REQUESTED_WITH'] != 'XMLHttpRequest'){
+    header("HTTP/1.0 404 Not Found");
+    header("Location: /404");
+    //return;
+}
+
+// Если в массиве POST нет действия - выход
+if (empty($_POST)) {return;}
+
+$message = '';
+
+if (isset($_POST['name'])){
+    $name = $_POST['name'];
+    $message .="Имя: $name \r\n";
+}
+if (isset($_POST['phone'])){
+    $phone = $_POST['phone'];
+    $message .="Телефон: $phone \r\n";
+}
+
+
+
+
+$result = mail("", "Сообщение из формы сайта Недвижимость", $message); 
+
+if($result === true){
+    echo "Done";
+}else{
+    echo "Error: " . $result;
+}
